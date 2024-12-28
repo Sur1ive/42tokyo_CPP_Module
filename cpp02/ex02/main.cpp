b7d5781e843b9237fa47282b25967078facede2a
yxu <penguinxx2@gmail.com>
2024-12-28 17:03:56 +0900

    recover cpp02/ex02/main

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
 #include "Fixed.hpp"
 
-// int main( void ) {
-
-//   Fixed a;
-//   Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-
-//   std::cout << a << std::endl;
-//   std::cout << ++a << std::endl;
-//   std::cout << a << std::endl;
-//   std::cout << a++ << std::endl;
-//   std::cout << a << std::endl;
-
-//   std::cout << b << std::endl;
-
-//   std::cout << Fixed::max( a, b ) << std::endl;
-
-//   return 0;
-// }
-
 int main( void ) {
 
   Fixed a;
@@ -32,7 +14,7 @@ int main( void ) {
 
   std::cout << b << std::endl;
 
-  std::cout << (Fixed(1.01f) != Fixed(1)) << std::endl;
+  std::cout << Fixed::max( a, b ) << std::endl;
 
   return 0;
 }

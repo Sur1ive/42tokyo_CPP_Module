bf34700fba9e51d4493632270e2b8762394317fe
yxu <penguinxx2@gmail.com>
2025-06-14 16:45:17 +0900

    cpp05/ex00: improve directory structure

--- cpp05/ex00/src/main.cpp
+++ cpp05/ex00/src/main.cpp
@@ -4,7 +4,7 @@ int main() {
 	std::cout << "----------------Test 1------------------" << std::endl;
 	try {
 		std::cout << "Creating bureaucrat with grade 151" << std::endl;
-		Bureaucrat bureaucrat("test", 151);
+		Bureaucrat bureaucrat("TestName", 151);
 		std::cout << bureaucrat << std::endl;
 	} catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
@@ -12,7 +12,7 @@ int main() {
 	std::cout << "----------------Test 2------------------" << std::endl;
 	try {
 		std::cout << "Creating bureaucrat with grade 0" << std::endl;
-		Bureaucrat bureaucrat("test", 0);
+		Bureaucrat bureaucrat("TestName", 0);
 		std::cout << bureaucrat << std::endl;
 	} catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
@@ -20,7 +20,7 @@ int main() {
 	std::cout << "----------------Test 3------------------" << std::endl;
 	try {
 		std::cout << "Creating bureaucrat with grade 1" << std::endl;
-		Bureaucrat bureaucrat("test", 1);
+		Bureaucrat bureaucrat("TestName", 1);
 		std::cout << bureaucrat << std::endl;
 		std::cout << "Incrementing grade" << std::endl;
 		bureaucrat.incrementGrade();
@@ -31,7 +31,7 @@ int main() {
 	std::cout << "----------------Test 4------------------" << std::endl;
 	try {
 		std::cout << "Creating bureaucrat with grade 150" << std::endl;
-		Bureaucrat bureaucrat("test", 150);
+		Bureaucrat bureaucrat("TestName", 150);
 		std::cout << bureaucrat << std::endl;
 		std::cout << "Decrementing grade" << std::endl;
 		bureaucrat.decrementGrade();
@@ -42,7 +42,7 @@ int main() {
 	std::cout << "----------------Test 5------------------" << std::endl;
 	try {
 		std::cout << "Creating bureaucrat with grade 150" << std::endl;
-		Bureaucrat bureaucrat("test", 150);
+		Bureaucrat bureaucrat("TestName", 150);
 		std::cout << bureaucrat << std::endl;
 		std::cout << "Incrementing grade" << std::endl;
 		bureaucrat.incrementGrade();

6db577fe2c915f8ec23a093d482d4421c7621df7
yxu <penguinxx2@gmail.com>
2025-06-23 23:48:54 +0900

    improve cpp07/ex01

--- cpp07/ex01/src/iter.hpp
+++ cpp07/ex01/src/iter.hpp
@@ -2,16 +2,11 @@
 #define ITER_HPP
 
 #include <cstddef>
+#include <stdexcept>
 
-template <typename T, typename R>
-void iter(T *array, size_t length, R (*f)(T &)) {
-  for (size_t i = 0; i < length; i++) {
-    f(array[i]);
-  }
-}
-
-template <typename T, typename R>
-void iter(T *array, size_t length, R (*f)(const T &)) {
+template <typename T, typename F> void iter(T *array, size_t length, F f) {
+  if (!array)
+    throw std::runtime_error("iter: array or function is null");
   for (size_t i = 0; i < length; i++) {
     f(array[i]);
   }
--- cpp07/ex01/src/main.cpp
+++ cpp07/ex01/src/main.cpp
@@ -1,24 +1,35 @@
 #include "iter.hpp"
 #include <iostream>
 
-void inc(int &x) { ++x; }
+void inc(int &x) {
+  ++x;
+  std::cout << x << ' ';
+}
 
 void print(const int &x) { std::cout << x << ' '; }
 
 int main() {
   int arr[5];
+  const int arr_const[5] = {1, 2, 3, 4, 5};
+
   for (int i = 0; i < 5; ++i)
     arr[i] = i + 1;
 
-  std::cout << "Original : ";
+  std::cout << "non-const array and const function: \n";
   iter(arr, 5, print);
   std::cout << std::endl;
 
+  std::cout << "non-const array and non-const function: \n";
   iter(arr, 5, inc);
-
-  std::cout << "After inc: ";
-  iter(arr, 5, print);
   std::cout << std::endl;
+
+  std::cout << "const array and const function: \n";
+  iter(arr_const, 5, print);
+  std::cout << std::endl;
+
+  // std::cout << "const array and non-const function:(cannot compile) ";
+  // iter(arr_const, 5, inc);
+  // std::cout << std::endl;
 
   return 0;
 }
